4a14a9e6512f93c98018b7a1fe69041e5ed9813c
toritori0318 <toritori0318@gmail.com>
2013-01-03 00:10:04 +0900

    Added support for 'zones' option with 'region'

--- lib/Yogafire/Regions.pm
+++ lib/Yogafire/Regions.pm
@@ -11,6 +11,7 @@ my @regions = (
     { id => 'us-west-2',      name => 'US West (Oregon)' },
     { id => 'eu-west-1',      name => 'EU (Ireland)' },
     { id => 'ap-southeast-1', name => 'Asia Pacific (Singapore)' },
+    { id => 'ap-southeast-2', name => 'Asia Pacific (Sydney)' },
     { id => 'ap-northeast-1', name => 'Asia Pacific (Tokyo)' },
     { id => 'sa-east-1',      name => 'South America (Sao Paulo)' },
 );
@@ -21,20 +22,24 @@ use Term::ANSIColor qw/colored/;
 sub list { \@regions; }
 
 sub display_table {
+    my ($rows, $zones) = @_;
     my @header = qw/region_id region_name/;
+    push @header, 'region_zones' if $zones;
     my $t = Text::ASCIITable->new();
     $t->setCols(@header);
 
     my $no = 0;
-    for my $row (@regions) {
-        $t->addRow([$row->{id}, $row->{name}]);
+    for my $row (@$rows) {
+        my @data = ($row->{id}, $row->{name});
+        push @data, join(',', @{$row->{zones}}) if $zones;
+        $t->addRow(\@data);
     }
     print $t;
 }
 
 sub find {
     my ($id) = @_;
-    my @search = grep { /^$id$/ } @regions;
+    my @search = grep { $_->{id} =~ /^$id$/ } @regions;
     return shift @search;
 }
 
