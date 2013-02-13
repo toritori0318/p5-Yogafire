74a0ad4fe15a28a9c4e9847340e3d573a4fbfd74
toritori0318 <toritori0318@gmail.com>
2013-02-14 01:03:42 +0900

    update instance-type info.

--- lib/Yogafire/InstanceTypes.pm
+++ lib/Yogafire/InstanceTypes.pm
@@ -6,19 +6,21 @@ use base 'Exporter';
 our @EXPORT_OK = qw/list display_table find/;
 
 my @instance_types = (
-    { id => 't1.micro',    name => 'Micro Instance',                                 cpu => '2ECU',        memory => '613MB',  io => 'Low',      price => '0.020', } ,
-    { id => 'm1.small',    name => 'Small Instance',                                 cpu => '1ECU x 1',    memory => '1.7GB',  io => 'Moderate', price => '0.080', } ,
-    { id => 'm1.medium',   name => 'Medium Instance',                                cpu => '2ECU x 1',    memory => '3.75GB', io => 'Moderate', price => '0.160', } ,
-    { id => 'm1.large',    name => 'Large Instance',                                 cpu => '2ECU x 2',    memory => '7.5GB',  io => 'High',     price => '0.320', } ,
-    { id => 'm1.xlarge',   name => 'Extra Large Instance',                           cpu => '2ECU x 4',    memory => '15GB',   io => 'High',     price => '0.640', } ,
-    { id => 'm2.xlarge',   name => 'High-Memory Extra Large Instance',               cpu => '3.25ECU x 2', memory => '17.1GB', io => 'Moderate', price => '0.450', } ,
-    { id => 'm2.2xlarge',  name => 'High-Memory Double Extra Large Instance',        cpu => '3.25ECU x 4', memory => '34.2GB', io => 'High',     price => '0.900', } ,
-    { id => 'm2.4xlarge',  name => 'High-Memory Quadruple Extra Large Instance',     cpu => '3.25ECU x 8', memory => '68.4GB', io => 'High',     price => '1.800', } ,
-    { id => 'c1.medium',   name => 'High-CPU Medium Instance',                       cpu => '2.5ECU x 2',  memory => '1.7GB',  io => 'Moderate', price => '0.165', } ,
-    { id => 'c1.xlarge',   name => 'High-CPU Extra Large Instance',                  cpu => '2.5ECU x 8',  memory => '7GB',    io => 'High',     price => '0.660', } ,
-    { id => 'cc1.4xlarge', name => 'Cluster Compute Quadruple Extra Large Instance', cpu => '2 x Intel Xeon X5570, quad-core “Nehalem” architecture',        memory => '23GB',   io => 'Very High', price => '1.300', } ,
-    { id => 'cc2.8xlarge', name => 'Cluster Compute Quadruple Extra Large Instance', cpu => '2 x Intel Xeon E5-2670, eight-core "Sandy Bridge" architecture',  memory => '60.5GB', io => 'Very High', price => '2.400', } ,
-    { id => 'cg1.4xlarge', name => 'Cluster GPU Quadruple Extra Large Instance'    , cpu => '2 x Intel Xeon X5570, quad-core “Nehalem” architecture',        memory => '22GB',   io => 'Very High', price => '2.100', } ,
+    { id => 't1.micro',    name => 'Micro Instance',                                 cpu => '2ECU',        memory => '613MB',  io => 'Low',      price => '0.027', } ,
+    { id => 'm1.small',    name => 'Small Instance',                                 cpu => '1ECU x 1',    memory => '1.7GB',  io => 'Moderate', price => '0.088', } ,
+    { id => 'm1.medium',   name => 'Medium Instance',                                cpu => '2ECU x 1',    memory => '3.75GB', io => 'Moderate', price => '0.175', } ,
+    { id => 'm1.large',    name => 'Large Instance',                                 cpu => '2ECU x 2',    memory => '7.5GB',  io => 'High',     price => '0.350', } ,
+    { id => 'm1.xlarge',   name => 'Extra Large Instance',                           cpu => '2ECU x 4',    memory => '15GB',   io => 'High',     price => '0.700', } ,
+    { id => 'm2.xlarge',   name => 'High-Memory Extra Large Instance',               cpu => '3.25ECU x 2', memory => '17.1GB', io => 'Moderate', price => '0.505', } ,
+    { id => 'm2.2xlarge',  name => 'High-Memory Double Extra Large Instance',        cpu => '3.25ECU x 4', memory => '34.2GB', io => 'High',     price => '1.010', } ,
+    { id => 'm2.4xlarge',  name => 'High-Memory Quadruple Extra Large Instance',     cpu => '3.25ECU x 8', memory => '68.4GB', io => 'High',     price => '2.020', } ,
+    { id => 'm3.xlarge',   name => 'M3 Extra Large Instance',                        cpu => '3.25ECU x 4', memory => '15GB',   io => 'Moderate', price => '0.760', } ,
+    { id => 'm3.2xlarge',  name => 'M3 Double Extra Large Instance',                 cpu => '3.25ECU x 8', memory => '30GB',   io => 'High',     price => '1.520', } ,
+    { id => 'c1.medium',   name => 'High-CPU Medium Instance',                       cpu => '2.5ECU x 2',  memory => '1.7GB',  io => 'Moderate', price => '0.185', } ,
+    { id => 'c1.xlarge',   name => 'High-CPU Extra Large Instance',                  cpu => '2.5ECU x 8',  memory => '7GB',    io => 'High',     price => '0.740', } ,
+    { id => 'cc1.4xlarge', name => 'Cluster Compute Quadruple Extra Large Instance', cpu => '2 x Intel Xeon X5570, quad-core "Nehalem" arch',          memory => '23GB',   io => 'Very High', price => '-', } ,
+    { id => 'cc2.8xlarge', name => 'Cluster Compute Quadruple Extra Large Instance', cpu => '2 x Intel Xeon E5-2670, eight-core "Sandy Bridge" arch',  memory => '60.5GB', io => 'Very High', price => '-', } ,
+    { id => 'cg1.4xlarge', name => 'Cluster GPU Quadruple Extra Large Instance'    , cpu => '2 x Intel Xeon X5570, quad-core "Nehalem" arch',          memory => '22GB',   io => 'Very High', price => '-', } ,
     { id => 'hi1.4xlarge', name => 'High I/O Quadruple Extra Large Instance'       , cpu => '4.4ECU x 8', memory => '60.5GB', io => 'Extremely high', price => '3.100', } ,
 );
 use Text::ASCIITable;
@@ -43,12 +45,19 @@ sub find {
     $cond ||= {};
 
     my @search;
-    if($cond->{id}) {
-        @search = grep { $_->{id} =~ /^$cond->{id}$/ } @instance_types;
-    } elsif($cond->{name}) {
-        @search = grep { $_->{name} =~ /^$cond->{name}$/ } @instance_types;
+    for my $key (qw/id name/) {
+        next unless $cond->{$key};
+
+        for (@{build_instance_types()}) {
+            my $list_val = $_->{$key};
+            my $cond_val = quotemeta($cond->{$key});
+            if($list_val =~ /$cond_val/) {
+                push @search, $_;
+            }
+        }
     }
     return shift @search;
 }
+
 
 1;
