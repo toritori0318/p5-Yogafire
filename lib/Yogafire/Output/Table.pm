57bb7fb8d374c286ffd5e63a5eea5787beb07dd0
toritori0318 <toritori0318@gmail.com>
2013-03-21 02:32:43 +0900

    Changed Text::ASCIITable to Text::UnicodeTable::Simple

--- lib/Yogafire/Output/Table.pm
+++ lib/Yogafire/Output/Table.pm
@@ -5,15 +5,17 @@ use Mouse;
 has 'header' => (is => 'rw');
 no Mouse;
 
-use Text::ASCIITable;
+use Text::UnicodeTable::Simple;
 use Term::ANSIColor qw/colored/;
 
 sub output {
     my ($self, $rows) = @_;
     my $data_header = $self->header;
     my @disp_header = ('no', @$data_header);
-    my $t = Text::ASCIITable->new();
-    $t->setCols(@disp_header);
+    my $t = Text::UnicodeTable::Simple->new(
+        header     => \@disp_header,
+        ansi_color => 1,
+    );
 
     my $no = 0;
     for my $row (@$rows) {
