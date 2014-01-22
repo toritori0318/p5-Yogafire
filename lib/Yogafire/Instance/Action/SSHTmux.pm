421e6d706b257d6c48a2505558c0defe63fc370b
toritori0318 <toritori0318@gmail.com>
2014-01-23 04:05:20 +0900

    Change the internal workings of "ssh-tmux" command

--- lib/Yogafire/Instance/Action/SSHTmux.pm
+++ lib/Yogafire/Instance/Action/SSHTmux.pm
@@ -17,6 +17,7 @@ no Mouse;
 
 use Yogafire::Instance qw/list/;
 use Yogafire::CommandClass::SSH;
+use POSIX qw(strftime);
 
 sub procs {
     my ($self, $instances, $opt) = @_;
@@ -30,6 +31,7 @@ sub proc {
 
 sub _procs {
     my ($self, $instances, $opt) = @_;
+    $instances = [$instances] if ref($instances) ne 'ARRAY';
 
     my @ssh_commands;
     for my $instance (@$instances) {
@@ -40,16 +42,29 @@ sub _procs {
         );
         my $ssh_cmd = $yoga_ssh->build_raw_ssh_command($instance);
         if(scalar @ssh_commands == 0) {
-            push @ssh_commands, sprintf('tmux new-window -n "yoga" "%s" ', $ssh_cmd);
+            push @ssh_commands, sprintf('tmux new-window -n "yoga-ssh-win" "%s"', $ssh_cmd);
         } else {
-            push @ssh_commands, sprintf(" tmux split-window '%s' && tmux select-layout 'tile' ", $ssh_cmd);
+            push @ssh_commands, sprintf("tmux split-window '%s' && tmux select-layout 'tile'", $ssh_cmd);
         }
     }
 
-    my $tmux_ssh_command = join("&&", @ssh_commands);
+    my $session = '';
+    if($ENV{SESSION_NAME}) {
+        $session=$ENV{SESSION_NAME};
+    } else {
+        my $datetime = strftime("%Y%m%d%H%M%S", localtime());
+        $session="yoga-ssh-$datetime";
+    }
+
+    # start tmux session
+    system("tmux new-session -d -n yoga-ssh-tmux -s $session");
+
+    # exec tmux attach
+    push @ssh_commands, "tmux select-pane -t 1";
+    push @ssh_commands, "tmux attach-session -t $session";
+    my $tmux_ssh_command = join(" && ", @ssh_commands);
     #print "$tmux_ssh_command\n";
     exec($tmux_ssh_command);
 };
 
 1;
-
